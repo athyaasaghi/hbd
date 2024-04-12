f4f85a982ea8929123e9902f63d2629a884a8382
athyaasaghi <asaghiathya@gmail.com>
2024-04-12 08:59:22 +0700

    cape

--- script/main.js
+++ script/main.js
@@ -102,10 +102,6 @@ const animationTimeline = () => {
         },
         0.05
     )
-    .to(".fake-btn", 0.1, {
-        backgroundColor: "rgb(127, 206, 248)",
-    },
-    "+=4")
     .to(
         ".four",
         0.5, {
